ab58010054ae306451fbaaeb4b1eec9aa8a8fc0a
AzizAkbarov <azizbakbarov@gmail.com>
2024-06-05 11:01:20 +0500

    Handling side effects

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,29 @@
 import Post from "./Post"
 import classes from './PostsList.module.css';
 import NewPost from "./NewPost";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Modal from "./Modal";
 
 function PostsList({isPosting, onStopPosting}) {
   const [posts, setPosts] = useState([])
 
+  useEffect(() => {
+        async function fetchPosts() {
+            const response = await fetch('http://localhost:8080/posts')
+            const resData = await response.json();
+            setPosts(resData.posts);
+        }
+        fetchPosts();
+  }, [])
+
   function addPostHandler(postData){
+    fetch('http://localhost:8080/posts', {
+        method:'POST',
+        body: JSON.stringify(postData),
+        headers: {
+            'Content-type': 'application/json'
+        }
+    } )
     setPosts(existingPosts => [postData, ...existingPosts])
   }
     return(
