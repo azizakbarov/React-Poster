4fcad19d43cd4ebf66f3f73a3f8a14974b5bfa18
AzizAkbarov <azizbakbarov@gmail.com>
2024-06-06 11:33:35 +0500

    Linking

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,17 +1,19 @@
 import Post from "./Post"
 import classes from './PostsList.module.css';
-import NewPost from "./NewPost";
 import { useState, useEffect } from "react";
-import Modal from "./Modal";
 
-function PostsList({isPosting, onStopPosting}) {
-  const [posts, setPosts] = useState([])
+
+function PostsList() {
+  const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
         async function fetchPosts() {
-            const response = await fetch('http://localhost:8080/posts')
+            setIsFetching(true);
+            const response = await fetch('http://localhost:8080/posts');
             const resData = await response.json();
             setPosts(resData.posts);
+            setIsFetching(false);
         }
         fetchPosts();
   }, [])
@@ -28,25 +30,22 @@ function PostsList({isPosting, onStopPosting}) {
   }
     return(
         <>  
-            {isPosting ? 
-                <Modal onClose ={onStopPosting}>
-                    <NewPost 
-                        onCancel={onStopPosting}
-                        onAddPost={addPostHandler} 
-                    />
-                </Modal> 
-            : false};
-            {posts.length > 0 && (
+            {!isFetching && posts.length > 0 && (
                 <ul className={classes.posts}>
                  {posts.map((post) => <Post key={post.body} author={post.author}  body={post.body}/>)}
                 </ul>
             )}
-           {posts.length === 0 && (
+           {!isFetching && posts.length === 0 && (
                 <div style={{textAlign:"center", color:'white'}}>
                     <h1>There are no posts yet</h1>
                     <p> Start adding some</p>
                 </div>
             )}
+            {isFetching && 
+                <div style={{textAlign: 'center', color:'white'}}> 
+                    <p> Loading posts...</p>
+                </div>
+            }
         </>
     )
 }
